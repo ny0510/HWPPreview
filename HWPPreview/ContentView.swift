2b85b5ff8da7132c6ad44083ac82ac008b631831
ny64 <me@ny64.kr>
2026-05-06 10:36:49 +0900

    Initial Commit

---
+++ HWPPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HWPPreview
+//
+//  Created by ny64 on 5/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HWPPreview/HWPPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWPPreviewApp.swift
+//  HWPPreview
+//
+//  Created by ny64 on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HWPPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
